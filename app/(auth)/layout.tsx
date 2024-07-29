30957c485a14d5905a9db45ddcdb76a86604fd6f
AnyaPolischuk <polisuk092@gmail.com>
2024-07-29 21:52:50 +0300

    feature: install auth with clerk

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,13 @@
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-    return ( 
-        <div className="bg-lime-800 h-full">
-            {children}
-        </div>
-     );
+const AuthLayout = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
+  return (
+    <div className="h-full flex items-center justify-center">
+      {children}
+    </div>
+  )
 }
- 
+
 export default AuthLayout;
